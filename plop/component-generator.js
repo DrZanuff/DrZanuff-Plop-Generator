c58fe9c2d8c118dfed3096adcaf24640d8b2cd47
Ricardo <ricardo.machado.nwi@gmail.com>
2023-09-04 18:21:47 -0300

    feat: added react native template

--- plop/component-generator.js
+++ plop/component-generator.js
@@ -4,18 +4,27 @@ module.exports = function (plop) {
     prompts: [
       {
         type: 'rawlist',
+        name: 'framework',
+        message: 'React ou React Native?',
+        choices: ['React', 'React Native'],
+        filter(val) {
+          return val == 'React Native' ? 'native' : 'react'
+        },
+      },
+      {
+        type: 'rawlist',
         name: 'language',
         message: 'Typescript ou Javascript?',
         choices: ['Typescript', 'Javascript'],
         filter(val) {
           return val == 'Typescript' ? 'ts' : 'js'
-        }
+        },
       },
       {
         type: 'input',
         name: 'name',
-        message: 'Nome do Componente'
-      }
+        message: 'Nome do Componente',
+      },
     ],
     actions: function (data) {
       const actions = []
@@ -23,28 +32,29 @@ module.exports = function (plop) {
       actions.push({
         type: 'add',
         path: `./src/components/{{name}}/index.{{language}}x`,
-        templateFile: './plop/component-index-template.hbs'
+        templateFile: './plop/{{framework}}/component-index-template.hbs',
       })
       actions.push({
         type: 'add',
         path: `./src/components/{{name}}/{{name}}.{{language}}x`,
-        templateFile: './plop/component-name-template-{{language}}.hbs'
+        templateFile:
+          './plop/{{framework}}/component-name-template-{{language}}.hbs',
       })
       actions.push({
         type: 'add',
         path: `./src/components/{{name}}/{{name}}.styles.{{language}}x`,
-        templateFile: './plop/component-styles-template.hbs'
+        templateFile: './plop/{{framework}}/component-styles-template.hbs',
       })
 
       if (data.language == 'ts') {
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.types.{{language}}x`,
-          templateFile: './plop/component-types-template.hbs'
+          templateFile: './plop/{{framework}}/component-types-template.hbs',
         })
       }
 
       return actions
-    }
+    },
   })
 }

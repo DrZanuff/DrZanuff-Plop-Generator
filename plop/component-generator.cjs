5f616d84ce92bac73b419494f0aa68b3dd7423ab
Ricardo <ricardo.machado.nwi@gmail.com>
2025-04-09 17:21:25 -0300

    fix path

--- plop/component-generator.cjs
+++ plop/component-generator.cjs
@@ -1,4 +1,8 @@
+const path = require('path')
+
 module.exports = function (plop) {
+  const templatesRoot = path.resolve(__dirname)
+
   plop.setGenerator('component', {
     description: 'Gerador de Componentes React',
     prompts: [
@@ -43,54 +47,73 @@ module.exports = function (plop) {
       actions.push({
         type: 'add',
         path: `./src/components/{{name}}/index.{{language}}x`,
-        templateFile: './plop/{{framework}}/component-index-template.hbs',
+        templateFile: path.join(
+          templatesRoot,
+          '{{framework}}/component-index-template.hbs'
+        ),
       })
 
-      if (data.styles == 'styled') {
+      if (data.styles === 'styled') {
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.{{language}}x`,
-          templateFile:
-            './plop/{{framework}}/component-name-template-{{language}}.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            '{{framework}}/component-name-template-{{language}}.hbs'
+          ),
         })
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.styles.{{language}}x`,
-          templateFile: './plop/{{framework}}/component-styles-template.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            '{{framework}}/component-styles-template.hbs'
+          ),
         })
       } else if (data.styles === 'cssModule') {
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.{{language}}x`,
-          templateFile:
-            './plop/react/component-name-template-{{language}}-cssmodule.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            'react/component-name-template-{{language}}-cssmodule.hbs'
+          ),
         })
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}-styles.module.css`,
-          templateFile:
-            './plop/react/component-styles-template-cssmodule.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            'react/component-styles-template-cssmodule.hbs'
+          ),
         })
       } else {
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.{{language}}x`,
-          templateFile:
-            './plop/{{framework}}/component-name-template-{{language}}-css.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            '{{framework}}/component-name-template-{{language}}-css.hbs'
+          ),
         })
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}-styles.css`,
-          templateFile:
-            './plop/{{framework}}/component-styles-template-css.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            '{{framework}}/component-styles-template-css.hbs'
+          ),
         })
       }
 
-      if (data.language == 'ts') {
+      if (data.language === 'ts') {
         actions.push({
           type: 'add',
           path: `./src/components/{{name}}/{{name}}.types.{{language}}x`,
-          templateFile: './plop/{{framework}}/component-types-template.hbs',
+          templateFile: path.join(
+            templatesRoot,
+            '{{framework}}/component-types-template.hbs'
+          ),
         })
       }
 
